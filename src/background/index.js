da4ebfc52b37910101aaead702bc00504b0c35fb
oscardavidrm <oscardavidrodriguezm@gmail.com>
2021-08-28 17:20:25 -0500

    yyy

--- src/background/index.js
+++ src/background/index.js
@@ -1,8 +1,11 @@
-// If your extension doesn't need a background script, just leave this file empty
+/*global chrome*/
 
 messageInBackground();
 
-// This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
+chrome.browserAction.onClicked.addListener(function(tab) {
+  alert(window.getSelection().toString());
+});
+
 export function messageInBackground() {
   console.log('I can run your javascript like any other code in your project');
   console.log('just do not forget, I cannot render anything !');
